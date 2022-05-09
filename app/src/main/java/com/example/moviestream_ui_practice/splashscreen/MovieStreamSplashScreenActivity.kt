9840e6f4ee20751499e0cc990abad1eb5d2c59c1
Roma Patel <roma.p@simformsolutions.com>
2022-05-09 20:19:44 +0530

    Sign Up Screen Done

---
+++ app/src/main/java/com/example/moviestream_ui_practice/splashscreen/MovieStreamSplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.moviestream_ui_practice.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.moviestream_ui_practice.MainActivity
+import com.example.moviestream_ui_practice.R
+
+class MovieStreamSplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_stream_splash_screen)
+
+        supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@MovieStreamSplashScreenActivity, MovieStreamOnBoardingActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)
+    }
+}
---
+++ app/src/main/java/com/example/moviestream_ui_practice/utils/Constant.kt
@@ -0,0 +1,14 @@
+package com.example.moviestream_ui_practice.utils
+
+object Constant  {
+    const val FOUR = 4
+    const val NINE = 9
+    const val TWO = 2
+    const val THIRTYTWO = 32
+    const val ZERO = 0
+    const val EIGHT = 8
+    const val FORTYTHREE = 43
+    const val FORTYONE = 41
+    val EMAILPATTERN : Regex = Regex("String emailPattern = \"[a-zA-Z0-9._-]+@[a-z]+\\\\.com+[a-z]+\"")
+    val PASSWORDPATTERN : Regex = Regex("String PasswordPatten = ^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%^&+=])(?=\\\\S+\$).{4,}\$")
+}
