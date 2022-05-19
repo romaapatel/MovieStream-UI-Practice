52210e389dd007b9a9412d430815a64c1f2edefd
Roma Patel <roma.p@simformsolutions.com>
2022-05-19 15:39:11 +0530

    description Screen Started

---
+++ app/src/main/java/com/example/moviestream_ui_practice/descriptionScreen/MovieStreamDescriptionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moviestream_ui_practice.descriptionScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moviestream_ui_practice.R
+
+class MovieStreamDescriptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_stream_description)
+    }
+}
